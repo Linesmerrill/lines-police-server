bf1d9b878b93200268b8e6bfda81953112aaa43c
Merrill Lines <linesmerrill@gmail.com>
2018-12-05 14:50:30 -0700

    delete cache. Removed broken sheriff veh. Add working sheriff veh to vMenu.

--- resources/[vehicles]/blaineCountySheriffPack/data/vehicle_names.lua
+++ resources/[vehicles]/blaineCountySheriffPack/data/vehicle_names.lua
@@ -4,14 +4,14 @@ end
 Citizen.CreateThread(function()
 	-- POLICE2
 	AddTextEntry('0x9F05F101', 'SHERIFF2')
-	-- POLICE3
-	AddTextEntry('0x71FA16EA', 'SHERIFF3')
+	-- BROKEN!! POLICE3
+	-- AddTextEntry('0x71FA16EA', 'SHERIFF3')
 	-- POLICE4
 	AddTextEntry('0x8A63C7B9', 'SHERIFF4')
 	-- POLICE5
-	AddTextEntry('0x9C32EB57', 'SHERIFF5') 		
+	AddTextEntry('0x9C32EB57', 'SHERIFF5')
 	-- POLICE6
-	AddTextEntry('0xB2FF98F0', 'SHERIFF6') 	
+	AddTextEntry('0xB2FF98F0', 'SHERIFF6')
 	-- POLICE7
 	AddTextEntry('0xC4B53C5B', 'SHERIFF7')
 	-- POLICE8
