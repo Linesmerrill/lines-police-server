870f75a022b6d99b2a16c103bab1dac6f4cefc02
Merrill Lines <linesmerrill@gmail.com>
2018-12-05 14:16:24 -0700

    update engineToggle to not automatically turn on when entering veh. Also requires that you use the /engine to toggle not a key

--- resources/[mods]/EngineToggle/config.lua
+++ resources/[mods]/EngineToggle/config.lua
@@ -1,8 +1,8 @@
 -- Change 'false' to 'true' to toggle the engine automatically on when entering a vehicle
-OnAtEnter = true
+OnAtEnter = false
 
 -- Change 'false' to 'true' to use a key instead of a button
-UseKey = true
+UseKey = false
 
 if UseKey then
 	-- Change this to change the key to toggle the engine (Other Keys at wiki.fivem.net/wiki/Controls)
