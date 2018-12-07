bd6a5dba6a9ca9b55b8ce350315cb60b5596b0dc
Merrill Lines <linesmerrill@gmail.com>
2018-12-07 11:41:16 -0700

    Adds LSCS police car pack

---
+++ resources/[vehicles]/LSCS-Pack/__resource.lua
@@ -0,0 +1,12 @@
+
+files {
+	'data/vehicles.meta',
+	'data/carcols.meta',
+	'data/carvariations.meta',
+	'data/handling.meta',
+}
+
+data_file 'VEHICLE_METADATA_FILE' 'data/vehicles.meta'
+data_file 'CARCOLS_FILE' 'data/carcols.meta'
+data_file 'HANDLING_FILE' 'data/handling.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/carvariations.meta'
