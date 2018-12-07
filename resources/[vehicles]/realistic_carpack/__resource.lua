f85eca36a6117043e05b86b8358c0f37925430a4
Merrill Lines <linesmerrill@gmail.com>
2018-12-07 11:45:23 -0700

    Adds realistic_carpack

---
+++ resources/[vehicles]/realistic_carpack/__resource.lua
@@ -0,0 +1,14 @@
+
+files {
+	'vehicles.meta',
+	'carvariations.meta',
+	'carcols.meta',
+	'handling.meta'
+}
+
+-- specify data file entries to be added
+-- these entries are the same as content.xml in a DLC pack
+data_file 'VEHICLE_METADATA_FILE' 'vehicles.meta'
+data_file 'CARCOLS_FILE' 'carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'carvariations.meta'
+data_file 'HANDLING_FILE' 'handling.meta'
